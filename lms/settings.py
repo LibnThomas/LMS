3395c2708fa22c1c4099decef4b6bbae18057896
LibinThomas <libinthomasooo1@gmail.com>
2019-10-28 10:08:54 +0530

    whitenoise.storage.CompressedManifestStaticFilesStorage added

--- lms/settings.py
+++ lms/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
